0bc07e9870a3d18e236e49da3114bea86b283b08
cburger70 <67466067+cburger70@users.noreply.github.com>
2020-10-20 07:50:47 -0500

    asp.net project 3.4.2

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -7,6 +7,10 @@ using Microsoft.EntityFrameworkCore;
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
-    {   
+    { 
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
