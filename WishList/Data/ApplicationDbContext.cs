55445fcabbaa7e68d99a3970645c7a8d51a998d9
cburger70 <67466067+cburger70@users.noreply.github.com>
2020-10-20 07:47:03 -0500

    asp.net project 3.4.1

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {   
-        public ApplicationDbContext(DbContextOptions) : base(Options)
-        {
-
-        }
     }
 }
